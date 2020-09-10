eec9deba274da83f9d6e207d08c03aac26f88498
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2020-09-10 14:24:54 -0400

    Declared variables and added movement to AppleTree script

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
-public class NewBehaviourScript2 : MonoBehaviour
+public class AppleTree : MonoBehaviour
 {
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirection = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        // speed * time is a measure of the number of seconds since last frame
+        pos.x += speed * Time.deltaTime;
+        //assigns the new position that was calculated on line 26
+        transform.position = pos;
     }
 }
